abebebcc539aabd5c7aba4a78639e42e9c0a1042
Adrián Cruz Ramos <adriancrz@MacBook-Pro.local>
2020-07-19 15:42:01 -0500

    added initial styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Cards, Chart, CountryTicker } from './components';
+import styles from './App.module.css';
 
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className= {styles.cotainer}>
                 <Cards />
                 <CountryTicker />
                 <Chart />
