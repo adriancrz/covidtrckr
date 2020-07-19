4ff4dcdb3e7be56a9e516ea03acb6a1e83d23668
Adrián Cruz Ramos <adriancrz@MacBook-Pro.local>
2020-07-19 17:23:15 -0500

    added api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Cards, Chart, CountryTicker } from './components';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryTicker from './components/CountryTicker/CountryTicker';
+//import { Cards, Chart, CountryTicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 
 class App extends React.Component {
+
+    async componentDidMount(){
+        const data = await fetchData();
+        console.log(data);
+    }
     render() {
         return (
-            <div className= {styles.cotainer}>
+            <div className= {styles.container}>
                 <Cards />
                 <CountryTicker />
                 <Chart />
--- src/components/CountryTicker/index.js
+++ src/components/CountryTicker/index.js
@@ -1,3 +1,5 @@
 export { default as Cards } from './Cards/Cards';
 export { default as Chart } from './Chart/Chart';
 export { default as CountryTicker } from './CountryTicker/CountryTicker';
+
+
