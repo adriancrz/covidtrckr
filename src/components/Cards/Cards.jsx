b41d1a46665dd56d52b4d862cc15e33359fce28c
Adrián Cruz Ramos <adriancrz@MacBook-Pro.local>
2020-07-19 15:31:55 -0500

    added component directories and files. initial code for jsx files.

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { findAllByAltText } from '@testing-library/react';
+
+const Cards = () => {
+    return (
+        <h1> Cards </h1>
+
+    )
+}
+
+export default Cards;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { findAllByAltText } from '@testing-library/react';
+
+const Cards = () => {
+    return (
+        <h1> Chart </h1>
+
+    )
+}
+
+export default Cards;
