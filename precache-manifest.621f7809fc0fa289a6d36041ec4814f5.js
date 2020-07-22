4473fe61290522dac5f945a4c3e987e1a06547ca
Adrián Cruz Ramos <adriancrz@MacBook-Pro.local>
2020-07-21 20:15:44 -0500

    Updates

---
+++ precache-manifest.621f7809fc0fa289a6d36041ec4814f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a93f0dd5c28f7027c9951b2a0b2fe4",
+    "url": "/covidtrckr/index.html"
+  },
+  {
+    "revision": "cecdab70aa1c7b468e72",
+    "url": "/covidtrckr/static/css/main.deaaa14c.chunk.css"
+  },
+  {
+    "revision": "3cbb8f1eaf391fd9a5a7",
+    "url": "/covidtrckr/static/js/2.28291fab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtrckr/static/js/2.28291fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cecdab70aa1c7b468e72",
+    "url": "/covidtrckr/static/js/main.69813d3c.chunk.js"
+  },
+  {
+    "revision": "98eb2a31d87e9a16e5d2",
+    "url": "/covidtrckr/static/js/runtime-main.ca1e63ed.js"
+  },
+  {
+    "revision": "94863534c718d35997f6c96cf3441cb5",
+    "url": "/covidtrckr/static/media/image.94863534.png"
+  }
+]);
